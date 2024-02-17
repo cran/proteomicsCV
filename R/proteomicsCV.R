6410262b72f50cfecdd90097094e08c57ec92436
Alejandro Brenes <alejandro.brenes@live.com>
2024-02-17 21:00:02 +0000

    version 0.1.0

---
+++ R/proteomicsCV.R
@@ -0,0 +1,11 @@
+
+#' @export
+logCV<-function(data, log_transformed){
+  if(log_transformed=="no"){
+     cv=apply(log(data), 1, function(x) sqrt(exp(stats::sd(x, na.rm = TRUE)^2)-1))*100
+  }
+  else{
+    cv=apply(data, 1, function(x) sqrt(exp(stats::sd(x, na.rm = TRUE)^2)-1))*100
+  }
+  cv
+}
