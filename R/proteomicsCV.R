a2cc332d7643ecf0d353604aab91c798767edf79
Alejandro Brenes <abrenes@ed.ac.uk>
2024-08-16 12:00:02 +0000

    version 0.3.0

--- R/proteomicsCV.R
+++ R/proteomicsCV.R
@@ -1,13 +1,10 @@
 
 #' @importFrom stats sd
 #' @export
-protLogCV<-function(data, log_transformed){
-  if(log_transformed=="no"){
-     cv=apply(log(data), 1, function(x) sqrt(exp(stats::sd(x, na.rm = TRUE)^2)-1))*100
-  }
-  else{
-    cv=apply(data, 1, function(x) sqrt(exp(stats::sd(x, na.rm = TRUE)^2)-1))*100
-  }
+protLogCV<-function(logData, base){
+  val=log(base)
+  data=logData*val
+  cv=apply(data, 1, function(x) sqrt(exp(stats::sd(x, na.rm = TRUE)^2)-1))*100
   cv
 }
 
